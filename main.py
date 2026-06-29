381ca37e40d307e0bb3f2a8edc9b4d83373e0ddc
Bhavyaraj Solanki <bks41205@gmail.com>
2026-06-29 17:58:23 -0500

    Stroing stock prices from CSV

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import csv
 
+prices = {
+    "Date": [],
+    "AAPL": [],
+    "MSFT": []
+}
+
 with open("data/prices.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-        print(row)
+        prices["Date"].append(row["Date"])
+        prices["AAPL"].append(float(row["AAPL"]))
+        prices["MSFT"].append(float(row["MSFT"]))
+
+print(prices)
